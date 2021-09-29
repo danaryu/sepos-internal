216cd1a2966d939de6786e5c491ee89a35622a1f
danaRyu <dana40426@gmail.com>
2021-09-29 14:25:50 +0900

    Initial Commit  - BootRun 가능하도록 변경

---
+++ src/main/java/com/sinc/sepos/internal/SeposIntApplication.java
@@ -0,0 +1,15 @@
+package com.sinc.sepos.internal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
+public class SeposIntApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeposIntApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sinc/sepos/internal/SeposIntApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sinc.sepos.internal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeposIntApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
